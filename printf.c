48374215772c98b6b90042ff2c1839c0bdf7ffbb
rafyc <rapha.chemouni@gmail.com>
2022-03-13 16:17:28 +0100

    test

--- printf.c
+++ printf.c
@@ -1,5 +1,49 @@
 #include "main.h"
 
+/**
+ * _printf - maint function
+ * @format: main param
+ * Return : int
+*/
+
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	int j = 0;
+	va_list args;
+
+	struc func[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"i", print_int},
+		{"d", print_double},
+		{NULL, NULL}
+	};
+	
+	va_start(args, format);
+
+
+	while (format[i] != '\0')
+		if (format[i] != '%')
+	{
+		_putchar (format[i]);
+		i++;
+	}
+		else if (format[i] == '%')
+		{
+			while (*func[j].c == format[i + 1])
+			{
+				func[j].f(args);
+				j++;
+			}
+			_putchar('%');
+			i++
+		}
+return(0);		
+	
+
+}
+
 
 /**
 * print_char - fonction
@@ -43,48 +87,5 @@ void print_int(va_list args)
 
 void print_doublr(va_list args)
 {
-
-}
-
-
-/**
- * _printf - maint function
- * @format: main param
- * Return : int
-*/
-
-int _printf(const char *format, ...)
-{
-	int i = 0;
-	int j = 0;
-	va_list args;
-
-	print func[] = {
-		{"c", print_char},
-		{"s", print_str},
-		{"i", print_int},
-		{"d", print_double},
-		{NULL, NULL}
-	};
-	
-	va_start(args, format);
-
-
-	while (format[i] != '\0' && format[i] != '%')
-	{
-		_putchar (format[i]);
-		i++;
-	}
-		else
-			i++;
-
-		while (func[j].c != format[i + 1])
-			j++;
-
-		if (func[j].f(args) == format[i])
-		{
-			_putchar('%');
-			func[j].f(args);
-		}
 
 }
