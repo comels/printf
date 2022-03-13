ec36aaa281dd655fa7b0cd1a6a964d0685a5d461
rafyc <rapha.chemouni@gmail.com>
2022-03-13 16:18:22 +0100

    test

--- main.h
+++ main.h
@@ -1,4 +1,16 @@
 #ifndef _PRINTF_
 #define _PRINTF_
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+typedef struct func{
+	char*c;
+	void (f*)(va_list args);
+} struct;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
 #endif
