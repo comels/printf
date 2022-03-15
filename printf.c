aa786644befcc7c298e831bb8477674bf5946082
comels <come.l.s@hotmail.fr>
2022-03-15 12:09:21 +0000

    20

--- printf.c
+++ printf.c
@@ -10,33 +10,24 @@ int _printf(const char *format, ...)
 {
 	int i = 0, j = 0, n = 0;
 	va_list args;
-
 	print func[] = {
 		{"c", print_char}, {"s", print_str},
 		{"d", print_d}, {"i", print_d}, {"\0", NULL}
 	};
 	if (format == NULL)
 		return (-1);
-
 	va_start(args, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			n++;
-		}
+			_putchar(format[i]), n++;
 		else if (format[i + 1])
 		{
 			j = 0;
 			while (*(func[j].c) != '\0')
 			{
 				if (format[i + 1] == *(func[j].c))
-				{
-					n += func[j].f(args);
-					i++;
-					break;
-				}
+					n += func[j].f(args), i++, break;
 				j++;
 			}
 			if (*(func[j].c) == '\0')
