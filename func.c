6f238d69ec1c6cffaf44971ea90f001c15e443dc
rafyc <rapha.chemouni@gmail.com>
2022-03-15 13:57:30 +0100

    func

--- func.c
+++ func.c
@@ -46,44 +46,13 @@ int print_str(va_list args)
  * @args: param
  * Return: void
  */
-/**
+
 int print_d(va_list args)
-{
-	int Div = 1;
-	int n;
-	int i = 0;
-	unsigned int x = 0;
-	int max; 
-
-	n = va_arg(args, int);
-	x = n;
-
-	if (n == 0)
-		_putchar(48), i++;
-	else if (n < 0)
-	{
-		_putchar('-'), i++;
-		x = n * -1;
-	}
-	while (x / Div * 10 != 0)
-		Div = Div * 10;
-	Div /= 10;
-	while (Div > 0)
-	{
-		_putchar((x / Div) + 48);
-		i++;
-		x = x % Div;
-		Div = Div / 10;
-	}
-	return (i);
-}*/
-
-int print_d(va_list valist)
 {
 	unsigned int m;
 	int n, i, k, digits = 0;
 
-	n = va_arg(valist, int);
+	n = va_arg(args, int);
 	if (n < 0)
 	{
 		n *= -1;
